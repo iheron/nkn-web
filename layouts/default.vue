0dd5bc9fb2d1a084d9bfda9b46ce1789d2c1fe78
heron <peng.liu@nkn.org>
2019-06-19 18:57:03 +0800

    styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
-    <v-navigation-drawer class="border-right card-border" v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed floating app>
-
+    <v-navigation-drawer class="border-right card-border" v-model="drawer" :mini-variant="miniVariant"
+                         :clipped="clipped" fixed floating app>
       <v-toolbar flat class="transparent">
         <v-list>
           <v-list-tile>
@@ -23,10 +23,8 @@
           <v-flex xs12>
             <AddWallet></AddWallet>
           </v-flex>
-
         </v-layout>
       </v-container>
-
     </v-navigation-drawer>
     <v-toolbar :clipped-left="clipped" dark fixed app tabs color="indigo" dense>
       <v-toolbar-side-icon @click="drawer = !drawer">
@@ -40,21 +38,42 @@
         NKN WEB
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-tabs align-with-title color="transparent" right>
-        <v-tab href="#one">Item One</v-tab>
-        <v-tab href="#two">Item Two</v-tab>
-        <v-tab href="#three">Item Three</v-tab>
-        <v-tabs-slider color="pink"></v-tabs-slider>
-      </v-tabs>
+      <NodeStatus></NodeStatus>
+      <div class="divider-vertical mx-3"></div>
+      <v-badge color="transparent">
+        <span>Node version: v0.8.0-alpha-174-gd090</span>
+      </v-badge>
+      <template v-slot:extension>
+        <v-tabs align-with-title color="transparent">
+          <v-tab href="#dashboard" to="/">Dashboard</v-tab>
+          <v-tab href="#node_status">Node status</v-tab>
+          <v-tab href="#settings" to="/settings">Settings</v-tab>
+          <v-tabs-slider color="pink accent-3"></v-tabs-slider>
+        </v-tabs>
+      </template>
+      <div class="divider-vertical mx-3"></div>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>language</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-tile @click="">
+            <v-list-tile-title>English</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile @click="">
+            <v-list-tile-title>简体中文</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </v-toolbar>
     <v-content>
-      <v-container>
+      <v-container fluid grid-list-lg>
         <nuxt/>
       </v-container>
       <Footer/>
     </v-content>
-
-
   </v-app>
 </template>
 
@@ -63,35 +82,22 @@
   import Wallet from '~/components/wallet/Wallet.vue'
   import Wallet2 from '~/components/wallet/Wallet2.vue'
   import AddWallet from '~/components/wallet/AddWallet.vue'
+  import NodeStatus from '~/components/status/NodeStatus.vue'
+  import '~/styles/status.scss'
+
   export default {
     components: {
       Footer,
       Wallet,
       Wallet2,
-      AddWallet
+      AddWallet,
+      NodeStatus
     },
     data() {
       return {
         clipped: false,
         drawer: true,
-        items: [
-          {
-            icon: 'apps',
-            title: 'Welcome',
-            to: '/'
-          },
-          {
-            icon: 'bubble_chart',
-            title: 'Inspire',
-            to: '/inspire'
-          }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-
-
+        miniVariant: false
       }
     }
   }
